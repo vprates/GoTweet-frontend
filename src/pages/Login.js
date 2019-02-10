0b21e62d0bb0a05e1c143473c3acc5ff761cb622
Vanildo Prates <vprates@gmail.com>
2019-02-10 15:29:18 -0200

    Frontend app

---
+++ src/pages/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import twitterlogo from '../twitter.svg';
+import './Login.css';
+
+export default class Login extends Component {
+  state = {
+    username: '',
+  };
+
+  inputHandleChange = e => {
+    this.setState({ username: e.target.value })
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem('@GoTwitter:username', username);
+
+    this.props.history.push('/timeline');
+  }
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <img src={twitterlogo} alt="GoTwitter" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange={this.inputHandleChange} 
+            placeholder="Nome de usuário" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
+  }
+}
